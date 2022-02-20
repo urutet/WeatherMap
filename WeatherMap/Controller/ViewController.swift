0621b055b82dbcc7eedf0240d0a6cd28982306a1
Ilya Yushkevich <ilyshka88@gmail.com>
2022-02-20 22:10:49 +0300

    location done

--- WeatherMap/Controller/ViewController.swift
+++ WeatherMap/Controller/ViewController.swift
@@ -16,10 +16,17 @@ final class ViewController: UIViewController {
     // MARK: Private
     private let map = MKMapView()
     private let locationManager = CLLocationManager()
-    
+    private let region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 53.893220, longitude: 27.556033),
+        latitudinalMeters: 10000,
+        longitudinalMeters: 10000
+    )
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Моя локация", style: .plain, target: self, action: #selector(goToDefaultLocation))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "cloud"), style: .plain, target: self, action: #selector(getWeather))
+        locationManager.requestWhenInUseAuthorization()
         setupUI()
         addSubviews()
         addConstraints()
@@ -28,6 +35,12 @@ final class ViewController: UIViewController {
     // MARK: - API
     // MARK: - Setups
     private func setupUI() {
+        map.setRegion(region, animated: true)
+        
+        let point = MKPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 53.8920185, longitude: 27.5551602)
+        point.title = "Belarusian State Technological University"
+        map.addAnnotation(point)
         
     }
     
@@ -41,8 +54,26 @@ final class ViewController: UIViewController {
         }
     }
     // MARK: - Helpers
+    @objc private func goToDefaultLocation() {
+        if let coordinate = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(
+                center: coordinate,
+                latitudinalMeters: 10000,
+                longitudinalMeters: 10000
+            )
+            map.setRegion(region, animated: true)
 
+        }
+    }
 
+    @objc private func getWeather() {
+        
+    }
+}
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        //get selected annotation title???
+    }
 }
 
